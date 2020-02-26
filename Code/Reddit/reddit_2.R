db77a139f1526b5da16e1e0a1be7a84c63564110
Joey O Brien <43303552+obrienjoey@users.noreply.github.com>
2020-02-26 11:57:30 +0000

    added sentiment check to reddit

--- Code/Reddit/reddit_2.R
+++ Code/Reddit/reddit_2.R
@@ -1,17 +1,19 @@
+
+
 file_name <- paste0('Output/reddit_ireland/',format(Sys.time(), "%Y%m%d_%H%M%S_"), "data_set.Rdata")
 
 reddit_ireland_scraper <- function(url){
   reddit_ireland <- xml2::read_html(url)
   
   times <- reddit_ireland %>%
-    html_nodes('a._3jOxDPIQ0KaOWpzvSQo-1s') %>%
-    html_text()
+    rvest::html_nodes('a._3jOxDPIQ0KaOWpzvSQo-1s') %>%
+    rvest::html_text()
   
   times
   
   urls <- reddit_ireland %>%
-    html_nodes('a._3jOxDPIQ0KaOWpzvSQo-1s') %>%
-    html_attr('href')
+    rvest::html_nodes('a._3jOxDPIQ0KaOWpzvSQo-1s') %>%
+    rvest::html_attr('href')
   
   urls
   
@@ -22,16 +24,16 @@ reddit_ireland_scraper <- function(url){
   comments <- c()
   for(i in reddit_ireland_df$Pages){
     
-    reddit_temp_data <- read_html(i)
+    reddit_temp_data <- xml2::read_html(i)
     body <- reddit_temp_data %>% 
-      html_nodes('p._1qeIAgB0cPwnLhDF9XSiJM') %>%
-      html_text()
+      rvest::html_nodes('p._1qeIAgB0cPwnLhDF9XSiJM') %>%
+      rvest::html_text()
     
     comments = append(comments, body)
     
     title <- reddit_temp_data %>%
-      html_node("title") %>%
-      html_text()
+      rvest::html_node("title") %>%
+      rvest::html_text()
     titles = append(titles, rep(title,each=length(body)))
     
   }
@@ -43,4 +45,9 @@ reddit_ireland_scraper <- function(url){
 reddit_ireland_scraper('https://www.reddit.com/r/ireland/new/') %>%
   save(., file = file_name)
 
+ireland_today <- reddit_ireland_scraper('https://www.reddit.com/r/ireland/new/')
+ireland_today %>% head()
+library(sentimentr)
+scores <- sentimentr::sentiment(ireland_today$Comments) %>% head()
 
+mean(scores$sentiment)
